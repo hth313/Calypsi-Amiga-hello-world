d53488d821e6e82c510d0574cc5d097d5fc80b1a
Håkan Thörngren <hth313@gmail.com>
2022-01-29 21:47:24 -0800

    Add some comments

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
+// Make use of Amiga dos.library, ideally this should just be an include,
+// but for now it is an include and a pragma.
 #include <clib/dos_protos.h>
-
 #pragma amiga library dos
 
 int main () {
